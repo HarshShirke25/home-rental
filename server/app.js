52a29e91c94d66e93d75d5c6d435fc880dfbba7c
HarshShirke25 <shirkeharsh8@gmail.com>
2022-01-17 23:18:02 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+require("./db/conn");
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running at port ${PORT}`);
+});
